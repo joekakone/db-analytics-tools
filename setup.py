4645d690fbb18d470c9038ab681f72e4d4810635
joekakone <joseph.kakone@gmail.com>
2023-09-16 18:35:35 +0000

    Generate URI in Client constructor

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="db_analytics_tools",
-    version="0.1.4.7",
+    version="0.1.4.8",
     url="https://josephkonka.com/#projects",
     download_url="https://github.com/joekakone/db-analytics-tools",
     project_urls={
