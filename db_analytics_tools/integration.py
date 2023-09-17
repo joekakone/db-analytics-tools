769807ced4555d4056be0707713251ebc824499f
joekakone <joseph.kakone@gmail.com>
2023-09-17 14:25:05 +0000

    Add Data Migration Functions

--- db_analytics_tools/integration.py
+++ db_analytics_tools/integration.py
@@ -4,7 +4,6 @@
     DB Analytics Tools Data Integration
 """
 
-
 import datetime
 
 import pandas as pd
@@ -13,18 +12,33 @@ from db_analytics_tools import Client
 
 
 class ETL:
-    """SQL Based ETL"""
+    """
+    SQL Based ETL (Extract, Transform, Load) Class
+
+    This class provides functionality for running SQL-based ETL processes using a database client.
+
+    :param client: An instance of the `Client` class for connecting to the database.
+    """
 
     def __init__(self, client):
         try:
             assert isinstance(client, Client)
         except Exception:
-            raise Exception("Something went wrong !")
+            raise Exception("Something went wrong!")
+
         self.client = client
 
     @staticmethod
     def generate_date_range(start_date, stop_date, freq='d', reverse=False):
-        """Generate Dates Range"""
+        """
+        Generate a range of dates.
+
+        :param start_date: The start date for the range.
+        :param stop_date: The stop date for the range.
+        :param freq: The frequency of the dates ('d' for daily, 'm' for monthly).
+        :param reverse: If True, the date range is generated in reverse order (from stop_date to start_date).
+        :return: A list of formatted date strings.
+        """
         dates_ranges = list(pd.date_range(start=start_date, end=stop_date, freq='d'))
 
         # Manage Frequency
@@ -36,7 +50,7 @@ class ETL:
                 for dt in dates_ranges if dt.strftime('%Y-%m-%d').endswith('01')
             ]
         else:
-            raise NotImplemented("Frequency not supported !")
+            raise NotImplementedError("Frequency not supported!")
 
         # Reverse
         if reverse:  # Recent to Old
@@ -48,17 +62,27 @@ class ETL:
         return dates_ranges
 
     def run(self, function, start_date, stop_date, freq='d', reverse=False, streamlit=False):
+        """
+        Run a specified SQL function for a range of dates.
+
+        :param function: The SQL function to run for each date.
+        :param start_date: The start date for the range.
+        :param stop_date: The stop date for the range.
+        :param freq: The frequency of the dates ('d' for daily, 'm' for monthly).
+        :param reverse: If True, the date range is generated in reverse order (from stop_date to start_date).
+        :param streamlit: If True, use Streamlit for progress updates.
+        """
         print(f'Function    : {function}')
 
         # Generate Dates Range
         dates_ranges = self.generate_date_range(start_date, stop_date, freq, reverse)
 
-        # Send query to server
+        # Send query to the server
         for date in dates_ranges:
-            print(f"[Runing Date: {date}] [Function: {function}] ", end="", flush=True)
+            print(f"[Running Date: {date}] [Function: {function}] ", end="", flush=True)
             if streamlit:
                 import streamlit as st
-                st.markdown(f"<span style='font-weight: bold;'>[Runing Date: {date}] [Function: {function}] </span>",
+                st.markdown(f"<span style='font-weight: bold;'>[Running Date: {date}] [Function: {function}] </span>",
                             unsafe_allow_html=True)
 
             query = f"select {function}('{date}'::date);"
@@ -67,7 +91,7 @@ class ETL:
             try:
                 self.client.execute(query)
             except Exception as e:
-                raise Exception("Something went wrong !")
+                raise Exception("Something went wrong!")
             finally:
                 self.client.close()
 
@@ -78,6 +102,16 @@ class ETL:
                             unsafe_allow_html=True)
 
     def run_multiple(self, functions, start_date, stop_date, freq='d', reverse=False, streamlit=False):
+        """
+        Run multiple specified SQL functions for a range of dates.
+
+        :param functions: A list of SQL functions to run for each date.
+        :param start_date: The start date for the range.
+        :param stop_date: The stop date for the range.
+        :param freq: The frequency of the dates ('d' for daily, 'm' for monthly).
+        :param reverse: If True, the date range is generated in reverse order (from stop_date to start_date).
+        :param streamlit: If True, use Streamlit for progress updates.
+        """
         print(f'Functions   : {functions}')
 
         # Compute MAX Length of functions (Adjust display)
@@ -86,16 +120,16 @@ class ETL:
         # Generate Dates Range
         dates_ranges = self.generate_date_range(start_date, stop_date, freq, reverse)
 
-        # Send query to server
+        # Send query to the server
         for date in dates_ranges:
-            ## Show date separator line
-            print("*"*(69+max_fun))
+            # Show date separator line
+            print("*" * (69 + max_fun))
             for function in functions:
-                print(f"[Runing Date: {date}] [Function: {function.ljust(max_fun, '.')}] ", end="", flush=True)
+                print(f"[Running Date: {date}] [Function: {function.ljust(max_fun, '.')}] ", end="", flush=True)
                 if streamlit:
                     import streamlit as st
                     st.markdown(
-                        f"<span style='font-weight: bold;'>[Runing Date: {date}] [Function: {function}] </span>",
+                        f"<span style='font-weight: bold;'>[Running Date: {date}] [Function: {function}] </span>",
                         unsafe_allow_html=True)
 
                 query = f"select {function}('{date}'::date);"
@@ -104,7 +138,7 @@ class ETL:
                 try:
                     self.client.execute(query)
                 except Exception as e:
-                    raise Exception("Something went wrong !")
+                    raise Exception("Something went wrong!")
                 finally:
                     self.client.close()
 
@@ -114,5 +148,27 @@ class ETL:
                     st.markdown(f"<span style='font-weight: bold;'>Execution time: {duration}</span>",
                                 unsafe_allow_html=True)
 
-        ## Show final date separator line
-        print("*"*(69+max_fun))
+        # Show final date separator line
+        print("*" * (69 + max_fun))
+
+
+def create_etl(host, port, database, username, password, engine):
+    """
+    Create an ETL (Extract, Transform, Load) instance with the specified database connection parameters.
+
+    :param host: The hostname or IP address of the database server.
+    :param port: The port number to use for the database connection.
+    :param database: The name of the database to connect to.
+    :param username: The username for authenticating the database connection.
+    :param password: The password for authenticating the database connection.
+    :param engine: The database engine to use, currently supports 'postgres' and 'sqlserver'.
+    :return: An ETL instance for performing data extraction, transformation, and loading.
+    """
+    client = Client(host=host,
+                    port=port,
+                    database=database,
+                    username=username,
+                    password=password,
+                    engine=engine)
+    etl = ETL(client)
+    return etl
