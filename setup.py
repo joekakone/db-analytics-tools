b04474f7ecc821518d04354ef1cc0ffd319d83c2
joekakone <joseph.kakone@gmail.com>
2025-04-13 15:50:22 +0000

    Fix Progression Prog..

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="db_analytics_tools",
-    version="0.1.7.6",
+    version="0.1.7.7",
     url="https://josephkonka.com/#projects",
     download_url="https://github.com/joekakone/db-analytics-tools",
     project_urls={
