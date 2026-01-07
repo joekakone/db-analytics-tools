4b4588053e76aaa14767fd036732e112e66e7c42
joekakone <joseph.kakone@gmail.com>
2026-01-07 13:39:02 +0000

    Airflow Data Migration utils

--- db_analytics_tools/airflow.py
+++ db_analytics_tools/airflow.py
@@ -9,6 +9,7 @@
 import urllib
 import datetime
 import json
+import os
 
 import requests
 from requests.auth import HTTPBasicAuth
@@ -401,3 +402,213 @@ class AirflowRESTAPI:
         
         return response
 
+
+def fetch_data(query, connection_id, output_file, html_file):
+    """
+    Fetches data from a database and saves it to a CSV file and an HTML file.
+    :param query: SQL query to execute.
+    :param connection_id: Database client.
+    :param output_file: Path to the output CSV file.
+    :param html_file: Path to the output HTML file.
+    """
+    # Get data
+    print("Get data")
+    df = connection_id.get_pandas_df(query)
+    print("Data ok")
+    df["color"] = df["nb_missing_dates"].apply(lambda x: "green" if x == 0 else "red")
+    
+    # Save data
+    df.to_csv(output_file, index=False, sep=";")
+    
+    # Generate HTML Table Rows
+    html = "\n".join([
+        f"""
+        <tr>
+            <td>{row.check_date}</td>
+            <td style="text-align: center;">{row.tableid}</td>
+            <td>bibox.{row.tablename}</td>
+            <td>{row.last_date}</td>
+            <td>{row.load_date}</td>
+            <td style="text-align: center; font-weight: bold; color: {row.color};">{row.status}</td>
+            <td style="text-align: center;">{row.nb_missing_dates}</td>
+        </tr>"""
+        for _, row in df.iterrows()
+    ])
+    # Save HTML
+    with open(html_file, "w") as f:
+        f.write(html)
+
+
+def extract_table_to_csv(table, connection_id):
+    """
+    Exports data from a source database table to a CSV file.
+    
+    :param table: Dictionary containing table information (src_table, dest_filename, sep).
+    :param connection_id: Database client.
+    """    
+    
+    # Get data
+    print("Get Data...")
+    df = connection_id.get_pandas_df(table["src_table"])
+    
+    # Save data
+    print("Save Data...", table["dest_filename"])
+    df.to_csv(table["dest_filename"], index=False, sep=table["sep"])
+
+
+def bulk_insert_dataframe(table, rundt, source_connection_id, destination_connection_id, mount_base_dir, win_base_dir, na_rep="", rollback_days=0):
+    """
+    Bulk insert data from source to destination database table using a temporary CSV file.
+    
+    :param table: Dictionary containing table information (src_table, dest_table, operation, date_column, format_file, is_daily, stratify).
+    :param rundt: The run date for data extraction.
+    :param source_connection_id: Source database client.
+    :param destination_connection_id: Destination database client.
+    :param mount_base_dir: Base directory for temporary file storage (Linux/Mac).
+    :param win_base_dir: Base directory for temporary file storage (Windows).
+    :param na_rep: Representation for missing values in the CSV file.
+    :param rollback_days: Number of days to roll back for monthly data extraction.
+    """    
+    
+    # Build Queries
+    print(f"\nTable : {table['src_table']} -> {table['dest_table']}")
+    if table["operation"] == "replace":
+        src_query = f"select * from {table['src_table']}"
+        dest_query = f"TRUNCATE TABLE {table['dest_table']};"
+    else:
+        if table.get('is_daily'):
+            src_query = f"select * from {table['src_table']} where {table['date_column']} = '{rundt}'::date"
+            dest_query = f"DELETE FROM {table['dest_table']} WHERE {table['date_column']} = cast('{rundt}' as DATE);"
+        else:
+            src_query = f"select * from {table['src_table']} where {table['date_column']} = date_trunc('month', '{rundt}'::date)::date"
+            # dest_query = f"DELETE FROM {table['dest_table']} WHERE {table['date_column']} = DATEADD(DAY, 1, EOMONTH(DATEADD(DAY, {rollback_days}, cast('{rundt}' as DATE)), -1));"
+            dest_query = f"DELETE FROM {table['dest_table']} WHERE {table['date_column']} = DATEADD(month, DATEDIFF(month, 0, cast('{rundt}' as DATE)), 0);"
+
+    # Get data
+    print("Get data")
+    print("Source query:", src_query)
+    if table.get('stratify'):
+        # Read data
+        uniques_sql = f"select distinct {table['stratify']} from ({src_query}) foo;"
+        uniques = source_connection_id.get_pandas_df(uniques_sql)
+        uniques = uniques[table['stratify']].tolist()
+
+        for i, v in enumerate(uniques):
+            src_query_sub = f"select * from ({src_query}) foo where {table['stratify']} = '{v}'"
+            print(src_query_sub)
+            df = source_connection_id.get_pandas_df(src_query_sub)
+
+            # Export data to CSV
+            print("Export data to CSV")
+            temp_filename = os.path.join(mount_base_dir, "tmp_" + table['dest_table'].split(".")[-1] + ".txt")
+            windows_temp_filename = os.path.join(win_base_dir, os.path.split(temp_filename)[-1])
+            if i == 0:
+                df.astype(str).to_csv(temp_filename, index=False, header=True, sep=";", lineterminator="\r\n", encoding="utf-8", na_rep=na_rep)
+            else:
+                df.astype(str).to_csv(temp_filename, index=False, header=False, sep=";", lineterminator="\r\n", encoding="utf-8", mode="a", na_rep=na_rep)
+    else:
+        # Read data
+        df = source_connection_id.get_pandas_df(src_query)
+
+        # Export data to CSV
+        print("Export data to CSV")
+        temp_filename = os.path.join(mount_base_dir, "tmp_" + table['dest_table'].split(".")[-1] + ".txt")
+        windows_temp_filename = os.path.join(win_base_dir, os.path.split(temp_filename)[-1])
+        df.astype(str).to_csv(temp_filename, index=False, header=True, sep=";", lineterminator='\r\n', encoding="utf-8", na_rep=na_rep)
+    
+    # Delete existing data
+    print("Delete existing data")
+    destination_connection_id.run(dest_query)
+    
+    # Insert data
+    print("Insert data")
+    sql = f"""
+    BULK INSERT {table['dest_table']}
+    FROM '{windows_temp_filename}'
+    WITH
+    (
+        FORMATFILE = '{os.path.join(win_base_dir, table['format_file'])}',
+        FIRSTROW = 2,
+        TABLOCK
+    )
+    """
+    destination_connection_id.run(sql)
+    
+    # Clean up temporary file
+    try:
+        os.remove(temp_filename)
+    except OSError as e:
+        print(f"Error: {temp_filename} : {e.strerror}")
+
+
+def bulk_insert_dataframe_db_to_db(table, rundt, source_connection_id, destination_connection_id, chunksize=10000):
+    """
+    Bulk insert data from source to destination database table using pandas DataFrame.
+    
+    :param table: Dictionary containing table information (src_table, dest_table, operation, date_column, format_file, is_daily, stratify).
+    :param rundt: The run date for data extraction.
+    :param source_connection_id: Source database client.
+    :param destination_connection_id: Destination database client.
+    :param chunksize: Number of rows per batch to insert.
+    """
+    # Build Queries
+    print(f"\nTable : {table['src_table']} -> {table['dest_table']}")
+    if table["operation"] == "replace":
+        src_query = f"select * from {table['src_table']}"
+        dest_query = f"truncate {table['dest_table']};"
+    else:
+        if table.get('is_daily'):
+            src_query = f"select * from {table['src_table']} where {table['date_column']} = '{rundt}'::date"
+            dest_query = f"delete from {table['dest_table']} WHERE {table['date_column']} = '{rundt}'::date;"
+        else:
+            src_query = f"select * from {table['src_table']} where {table['date_column']} = date_trunc('month', '{rundt}'::date)::date"
+            dest_query = f"delete from {table['dest_table']} WHERE {table['date_column']} = date_trunc('month', '{rundt}'::date)::date;"
+
+    if table.get('stratify'):
+        # Get data
+        print("Get data")
+        print("Source query:", src_query)
+        uniques_sql = f"select distinct {table['stratify']} from ({src_query}) foo;"
+        uniques = source_connection_id.get_pandas_df(uniques_sql)
+        uniques = uniques[table['stratify']].tolist()
+
+        for i, v in enumerate(uniques):
+            src_query_sub = f"select * from ({src_query}) foo where {table['stratify']} = '{v}'"
+            print(src_query_sub)
+            df = source_connection_id.get_pandas_df(src_query_sub)
+            
+            # Delete existing data
+            print("Delete existing data")
+            destination_connection_id.run(dest_query)
+    
+            # Insert data
+            df.to_sql(
+                name=table['dest_table'].split(".")[1],
+                schema=table['dest_table'].split(".")[0],
+                con=destination_connection_id.get_sqlalchemy_engine(),
+                if_exists="append", ## Ensure table is truncated if needed
+                chunksize=table.get('chunksize', chunksize),
+                method="multi",
+                index=False
+            )
+            
+    else:
+        # Get data
+        print("Get data")
+        print("Source query:", src_query)
+        df = source_connection_id.get_pandas_df(src_query)
+        
+        # Delete existing data
+        print("Delete existing data")
+        destination_connection_id.run(dest_query)
+    
+        # Insert data
+        df.to_sql(
+            name=table['dest_table'].split(".")[1],
+            schema=table['dest_table'].split(".")[0],
+            con=destination_connection_id.get_sqlalchemy_engine(),
+            if_exists="append", ## Ensure table is truncated if needed
+            chunksize=table.get('chunksize', chunksize),
+            method="multi",
+            index=False
+        )
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="db_analytics_tools",
-    version="0.1.8.4",
+    version="0.1.8.5",
     url="https://joekakone.github.io/#projects",
     download_url="https://github.com/joekakone/db-analytics-tools",
     project_urls={
