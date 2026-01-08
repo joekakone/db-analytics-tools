e745201bfb6dc89ebabb9b57472deda7004a4682
joekakone <joseph.kakone@gmail.com>
2026-01-08 08:58:20 +0000

    Update version 0.1.8.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="db_analytics_tools",
-    version="0.1.8.5",
+    version="0.1.8.6",
     url="https://joekakone.github.io/#projects",
     download_url="https://github.com/joekakone/db-analytics-tools",
     project_urls={
