c0753326a2c63bcb4bc6c577fcc0fcc7b51728e1
joekakone <joseph.kakone@gmail.com>
2026-05-13 18:47:40 +0000

    Fix  db_tools_cli

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="db_analytics_tools",
-    version="0.1.8.14",
+    version="0.1.8.15",
     url="https://joekakone.github.io/db-analytics-tools",
     download_url="https://github.com/joekakone/db-analytics-tools",
     project_urls={
