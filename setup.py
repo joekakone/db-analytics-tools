00702ed7f659ac4307bdabb024e20ca4f4b3c441
joekakone <joseph.kakone@gmail.com>
2026-06-03 21:29:11 +0000

    Prepare release v0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="db_analytics_tools",
-    version="0.1.9.9",
+    version="0.1.9.10",
     url="https://joekakone.github.io/db-analytics-tools",
     download_url="https://github.com/joekakone/db-analytics-tools",
     project_urls={
